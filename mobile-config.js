4ccae84e6a07c2f648b7956dfeee0efedcc2c7fc
Sean <s@kaulana.com>
2016-03-21 13:18:09 -0700

    Rev app version for new Meteor beta
    Removing deprecated mobile resource sizes

--- mobile-config.js
+++ mobile-config.js
@@ -1,30 +1,26 @@
 App.info({
   id: 'com.kaulana.scoretto',
-  version: '0.3.0',
+  version: '0.3.1',
   name: 'Scoretto',
-  description: 'A shared scorecard for Ligretto™ parties',
+  description: 'A shared scorecard for Dutch Blitz parties',
   author: 'kaulana.com',
   email: 'dev@kaulana.com',
   website: 'http://www.kaulana.com/support/?ref=scoretto'
 });
 
 App.icons({
-  'iphone': 'res/icon60.png',
   'iphone_2x': 'res/icon120.png',
   'iphone_3x': 'res/icon180.png',
-  'android_ldpi': 'res/icon36.png',
   'android_mdpi': 'res/icon48.png',
   'android_hdpi': 'res/icon72.png',
   'android_xhdpi': 'res/icon96.png'
 });
 
 App.launchScreens({
-  'iphone': 'res/splash320-480.png',
   'iphone_2x': 'res/splash640-960.png',
   'iphone5': 'res/splash640-1136.png',
   'iphone6': 'res/splash750-1334.png',
   'iphone6p_portrait': 'res/splash1242-2208.png',
-  'android_ldpi_portrait': 'res/splash200-320.png',
   'android_mdpi_portrait': 'res/splash320-480.png',
   'android_hdpi_portrait': 'res/splash480-800.png',
   'android_xhdpi_portrait': 'res/splash720-1280.png'
