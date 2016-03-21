a496046e05038af4edd98fffe09c1fb3470d32c3
Sean <s@kaulana.com>
2016-03-21 14:45:52 -0700

    Rev to 1.3-rc4 and update more mobile assets
    Removing iOS for now and adding additional Android sizes

--- mobile-config.js
+++ mobile-config.js
@@ -9,19 +9,18 @@ App.info({
 });
 
 App.icons({
-  'iphone_2x': 'res/icon120.png',
-  'iphone_3x': 'res/icon180.png',
   'android_mdpi': 'res/icon48.png',
   'android_hdpi': 'res/icon72.png',
-  'android_xhdpi': 'res/icon96.png'
+  'android_xhdpi': 'res/icon96.png',
+  'android_xxhdpi': 'res/icon144.png',
+  'android_xxxhdpi': 'res/icon192.png'
 });
 
 App.launchScreens({
-  'iphone_2x': 'res/splash640-960.png',
-  'iphone5': 'res/splash640-1136.png',
-  'iphone6': 'res/splash750-1334.png',
-  'iphone6p_portrait': 'res/splash1242-2208.png',
   'android_mdpi_portrait': 'res/splash320-480.png',
+  'android_mdpi_landscape': 'res/splash480-320.png',
   'android_hdpi_portrait': 'res/splash480-800.png',
-  'android_xhdpi_portrait': 'res/splash720-1280.png'
+  'android_hdpi_landscape': 'res/splash800-480.png',
+  'android_xhdpi_portrait': 'res/splash720-1280.png',
+  'android_xhdpi_landscape': 'res/splash1280-720.png'
 });
